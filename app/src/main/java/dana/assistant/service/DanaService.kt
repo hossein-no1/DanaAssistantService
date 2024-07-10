be5978666873b32c531aebee5ecd6bd8ed4233d9
hosen <hpleo0938@gmail.com>
2024-07-10 16:40:36 +0330

    remove permission and receive specify package name

--- app/src/main/java/dana/assistant/service/DanaService.kt
+++ app/src/main/java/dana/assistant/service/DanaService.kt
@@ -1,17 +1,22 @@
 package dana.assistant.service
 
 import android.content.Context
+import android.content.Context.RECEIVER_EXPORTED
 import android.content.Intent
 import android.content.IntentFilter
 import android.net.Uri
+import android.os.Build
+import androidx.annotation.RequiresApi
 
 class DanaService(private val context: Context) {
 
     private var assistantReceiver: AssistantBroadcastReceiver? = null
+    private var receiverPackageName = ""
 
     fun openAssistant(packageName: String, screenName: String) {
+        receiverPackageName = packageName
         val intent = Intent(Intent.ACTION_VIEW, Uri.parse("app://assistant.dana.ir/?page=overlay"))
-        intent.putExtra("client_package_name", packageName)
+        intent.putExtra("client_package_name", receiverPackageName)
         intent.putExtra("current_screen", screenName)
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
         try {
@@ -21,10 +26,11 @@ class DanaService(private val context: Context) {
         }
     }
 
-    fun registerService(callBack: AssistantCallBack, permission: String) {
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    fun registerService(callBack: AssistantCallBack) {
         assistantReceiver = AssistantBroadcastReceiver(callBack)
         val intentFilter = IntentFilter("dana.assistant.service.DETECT_COMMAND")
-        context.registerReceiver(assistantReceiver, intentFilter)
+        context.registerReceiver(assistantReceiver, intentFilter, RECEIVER_EXPORTED)
     }
 
     fun unregisterService() {
