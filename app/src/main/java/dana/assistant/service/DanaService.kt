ced67fea86b8f8d6079d1ec30fa632b6228110c0
hosen <hpleo0938@gmail.com>
2024-07-10 17:28:31 +0330

    change gradle

--- app/src/main/java/dana/assistant/service/DanaService.kt
+++ app/src/main/java/dana/assistant/service/DanaService.kt
@@ -1,12 +1,12 @@
 package dana.assistant.service
 
+import android.annotation.TargetApi
 import android.content.Context
 import android.content.Context.RECEIVER_EXPORTED
 import android.content.Intent
 import android.content.IntentFilter
 import android.net.Uri
 import android.os.Build
-import androidx.annotation.RequiresApi
 
 class DanaService(private val context: Context) {
 
@@ -26,7 +26,7 @@ class DanaService(private val context: Context) {
         }
     }
 
-    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    @TargetApi(Build.VERSION_CODES.O)
     fun registerService(callBack: AssistantCallBack) {
         assistantReceiver = AssistantBroadcastReceiver(callBack)
         val intentFilter = IntentFilter("dana.assistant.service.DETECT_COMMAND")
