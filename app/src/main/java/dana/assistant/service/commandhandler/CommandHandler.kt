c4d85721c31bc668deaec8f05f326fd7df578553
Kaaveh Mohamedi <Kaaveh@pm.me>
2024-12-17 12:16:43 +0330

    Set visibility for CommandHandler interface

--- app/src/main/java/dana/assistant/service/commandhandler/CommandHandler.kt
+++ app/src/main/java/dana/assistant/service/commandhandler/CommandHandler.kt
@@ -1,7 +1,9 @@
 package dana.assistant.service.commandhandler
 
+import androidx.annotation.RestrictTo
 import dana.assistant.service.model.CommandType
 
+@RestrictTo(RestrictTo.Scope.LIBRARY)
 interface CommandHandler {
     fun onReceiveCommand(commandType: CommandType, values: List<String>)
 }
