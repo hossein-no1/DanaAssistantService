9339be0beffbb329aeb4f66d6edd1c7c9f4ff22a
hosen <hpleo0938@gmail.com>
2024-12-20 17:32:54 +0330

    -make CommandHandler.kt open to use

--- app/src/main/java/dana/assistant/service/commandhandler/CommandHandler.kt
+++ app/src/main/java/dana/assistant/service/commandhandler/CommandHandler.kt
@@ -3,12 +3,13 @@ package dana.assistant.service.commandhandler
 import androidx.annotation.RestrictTo
 import dana.assistant.service.model.CommandType
 
-@RestrictTo(RestrictTo.Scope.LIBRARY)
-/**
- * Use «ContentDetailCommandHandler.kt»,«HomeCommandHandler.kt» and etc.
- * */
 interface CommandHandler {
-    fun onReceiveCommand(commandType: CommandType, values: List<String>){
+
+    @RestrictTo(RestrictTo.Scope.LIBRARY)
+            /**
+             * Use «ContentDetailCommandHandler.kt»,«HomeCommandHandler.kt» and etc.
+             * */
+    fun onReceiveCommand(commandType: CommandType, values: List<String>) {
         when (commandType) {
             CommandType.BackPress -> onBackPress()
             else -> Unit
